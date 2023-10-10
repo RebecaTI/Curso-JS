4a7305b4972390885a14d71fbad8e987f3e8bbe7
Rebeca Oliveira <mebuffbeka@gmail.com>
2023-10-10 12:14:56 -0300

    alterando

---
+++ 01-JavaScript-Objetos/aula1/alterar.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    nome: 'Luma',
+    profissao: 'Engenheira',
+}
+
+console.log(pessoa.nome)
+
+console.log(pessoa.telefone)
+
+console.log(pessoa.telefone = '11 222222')
+
+pessoa.nome = "Luma Silva"
+
+console.log(pessoa.nome)
