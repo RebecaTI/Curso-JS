2817c6666c179facf5e29c3fb392101022cf04fd
Rebeca Oliveira <mebuffbeka@gmail.com>
2023-10-10 11:24:15 -0300

    acessando objetos

--- 01-JavaScript-Objetos/aula1/objetos.js
+++ 01-JavaScript-Objetos/aula1/objetos.js
@@ -3,3 +3,8 @@ const listaCPFs = [111111, 22222, 33333];
 const informaçõesPessoas = ['nome', 'Jose', 'Idade', '32', 'CPF', '11122223333']
 
 console.log(informaçõesPessoas[1])
+
+const objetoPessoa = {
+    nome: "Jose",
+    idade: 32
+}
