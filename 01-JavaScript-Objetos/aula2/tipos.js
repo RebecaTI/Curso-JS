3f7514d1a74314b6b57b70bcbf13d56f345735c3
Rebeca Oliveira <mebuffbeka@gmail.com>
2023-10-10 12:59:33 -0300

    tipos

---
+++ 01-JavaScript-Objetos/aula2/tipos.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: 'jogao@firma.com',
+    telefone: ['133565','343545655'],
+}
+
+console.log(cliente.telefone[0])
+console.log(cliente.telefone[1])
