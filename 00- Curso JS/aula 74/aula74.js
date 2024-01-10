09eda618e4253bdc9c24855a895e669d571c09ea
Rebeca Oliveira <mebuffbeka@gmail.com>
2024-01-10 08:27:58 -0300

    a

--- 00- Curso JS/aula 74/aula74.js
+++ 00- Curso JS/aula 74/aula74.js
@@ -20,7 +20,7 @@ Pessoa.prototype.nomeCompleto = function () {
 
 // instancia 
 const pessoa1 = new Pessoa('Luiz', 'O.'); // <- Pessoa = Função construtora
-const pessoa2 = new Pessoa('Luiz', 'O.'); // <- Pessoa = Função construtora
+const pessoa2 = new Pessoa('Maria', 'A.'); // <- Pessoa = Função construtora
 const data = new Date()
 
 console.dir(pessoa1);
