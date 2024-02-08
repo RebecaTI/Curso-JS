164ab3557b6cadf747551221eea0365060730453
Rebeca Oliveira <mebuffbeka@gmail.com>
2024-02-08 08:49:14 -0300

    Preparação dos arquivos para adicionar ao meu site

--- 00- Curso JS/aulas/aula 80/aula80.js
+++ 00- Curso JS/aulas/aula 80/aula80.js
@@ -1,24 +1,23 @@
-const pessoas = [
-    {id:  3, nome: 'Luiz' },
-    {id:  2, nome: 'Maria' },
-    {id:  1, nome: 'Helena' },
-]
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
 
-// const novasPessoas = {};
-// for (const pessoa of pessoas) {
-//     const { id } = pessoa;
-//     novasPessoas[id] = {...pessoa};
-// }
-
-const novasPessoas = new Map();
-for (const pessoa of pessoas) {
-    const { id } = pessoa;
-    novasPessoas.set(id, {...pessoa});
+    falar() {
+        console.log(`${this.nome} está falando.`)
+    }
 }
 
-// console.log(novasPessoas.get(2))
-
-for (const pessoa of novasPessoas) {
-    console.log(pessoa);
+function Pessoa2 (nome,sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
 }
+
+Pessoa2.prototype.falar = function() {
+    console.log(`${this.nome} está falando.`)
+}
+
+const p1 = new Pessoa('Luiz', 'Miranda');
+const p2 = new Pessoa('Luiz', 'Miranda');
 
