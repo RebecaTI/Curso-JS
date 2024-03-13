d749ff1f3f49e08901309a3122b5e581728fe720
Rebeca Oliveira <mebuffbeka@gmail.com>
2024-03-13 11:04:40 -0300

    Estudo sobre o DOM, e inicio do primeiro projeto animais fantasticos

--- 00- Origamid/JavaScript ES6/Escopo de Função/script.js
+++ 00- Origamid/JavaScript ES6/Escopo de Função/script.js
@@ -4,4 +4,32 @@ function mostrarCarro() {
 }
 
 mostrarCarro();
-console.log(carro);
+// console.log(carro);
+
+if(false){
+  const mes ='Dezembro';
+  console.log(mes);
+}
+// console.log(mes);
+
+{
+  var carro2 = 'Fusca 2';
+  const ano = 2018;
+}
+console.log(carro2);
+// console.log(ano);
+
+// var i = 50;
+ 
+// for(let i =0; i< 10 ; i++){
+//   console.log(`Numero ${i}`);
+// }
+// console.log(i * 10);
+
+const data = {
+  ano:2018,
+  mes:'Dezembro',
+}
+
+data.ano = 2019;
+data.dia = 25;
